0f64bdce019d477ee030eb5a0ab2c0dffd3d1fcf
hvenses <hvenses@gmail.com>
2021-04-13 14:06:30 -0300

    Commit inicial :rocket:

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(port, () => {
+  console.log(`App rodando na porta ${port}`)
+})
