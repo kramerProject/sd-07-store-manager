0eab10665858656d1574233d7fedd4fcd6af4429
Fernando Antunes <fernando.antunes1@gmail.com>
2021-04-13 14:32:35 -0300

    criado productsController

---
+++ controllers/productsController.js
@@ -0,0 +1,17 @@
+
+
+const status = {
+  add: 201,
+  serverError: 500,
+};
+
+const addProduct = async (req, res) => {
+  try {
+    const { name, age } = req.body;
+    const newProduct = await productModel.add(name, age);
+    res.status(status.add).json(newPerson);
+  } catch (error) {
+    console.error(error.message);
+    res.status(status.serverError).json({ message: error.message });
+  }
+};
