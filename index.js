6078dd70238ad57186e6cd60db784aaa022a6425
Nonato <lucas.nonato1@hotmail.com>
2021-04-13 16:30:45 -0300

    branch creation

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const PORT = '3000';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
 });
