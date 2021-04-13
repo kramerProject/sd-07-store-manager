2ad8310547c1fb830b4a2b279418672790c67b64
Vitor Rodrigues <vitor_rc@outlook.com>
2021-04-13 16:14:09 -0300

    Iniciando o projeto

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const productsRoute = require('./routes/productsRoute')
+const app = express();
+
+app.use(express.json());
+app.use(productsRoute);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log("Ta rodando o menino"));
