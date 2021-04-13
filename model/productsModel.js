55e47a48040374f3279f13a8fa7f07c49be75a4e
Vitor Rodrigues <vitor_rc@outlook.com>
2021-04-13 17:06:36 -0300

    product Model

---
+++ model/productsModel.js
@@ -0,0 +1,33 @@
+const conn = require('../config/conn');
+
+const addProduct = async (name, quantity) => {
+  conn().then(async (db) => {
+    const product = await db.collection("products").insertOne({name, quantity});
+    return product.ops[0];
+  })
+}
+
+const getAllProducts = async () => conn()
+  .then((db) => db.collection('products').find().toArray());
+
+const getProductByName = async (name) => conn()
+  .then((db) => db.collection('products').findOne({name}));
+
+const updateProduct = async (id, name, quantity) => conn()
+  .then(async (db) => {
+    const product = db.collection('products').updateOne(
+      { _id: id },
+      { $set: {name, quantity}}
+    );
+  });
+
+  const deleteProduct = async (id) => conn()
+    .then(async (db) => db.collection('products').deleteOne({_id: id}));
+
+module.exports = {
+  addProduct,
+  getProductByName,
+  getAllProducts,
+  updateProduct,
+  deleteProduct,
+}
