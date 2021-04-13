0c19a9959930f6263a861c9d60661993c20836d4
marcelosoliveira <msbobsk8@gmail.com>
2021-04-13 14:17:12 -0300

    Criando branch

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || '3000';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
