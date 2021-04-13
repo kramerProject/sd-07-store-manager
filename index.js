2d374176f07f4f46257d47cb6c3ea5d2c7a56737
Lfocosi <luccafocosi@gmail.com>
2021-04-13 18:21:42 -0300

    iniciando o projeto

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT);
