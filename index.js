9386a08b3084dd2ab6021ddadb34f7bf181d6fb5
Fernando Antunes <fernando.antunes1@gmail.com>
2021-04-13 14:15:30 -0300

    Criação do litening no arquivo index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const port = 3000;
+app.listen(port, () => { console.log('Rodando...'); });
