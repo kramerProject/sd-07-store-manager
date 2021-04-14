fe9ddac4f3f5c97473ee16427c627de5d2715c83
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-04-14 10:50:09 -0300

    Configuracao inicial do index - importacao do express, porta e expressJSON

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const PORT = '3000';
+
+app.listen(PORT, () => {
+  console.log('online');
 });
