200ddcad4b35354e613b74e743852f4238aab3fd
renatomak <renato.mark.silva@gmail.com>
2021-04-14 16:21:13 -0300

    Requisito 3

--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.get('/products', productsController.getAllproducts);
 router.get('/products/:id', productsController.getProductById);
 router.post('/products', checkDateCreationMidlleware, productsController.addProduct);
-router.put('/products/:id', productsController.updateProduct);
+router.put('/products/:id',
+  checkDateCreationMidlleware,
+  productsController.updateProduct);
 router.delete('/products/:id', productsController.deleteProduct);
 
 module.exports = router;
