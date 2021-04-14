ee5c9de5df242a7f4f614ae062c14c6239a5046b
Lfocosi <luccafocosi@gmail.com>
2021-04-14 11:49:40 -0300

    bb steps, criação das pastas

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const products = require('./routers/products');
-const { sendError } = require('./configs/erro');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (_request, response) => {
   response.send();
 });
-
-app.use('/products', products);
 
 app.listen(PORT);
