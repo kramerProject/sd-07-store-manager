b74a9b10293ecab9e72a512e152713cb8ccf2386
julioKauer <juliokauer@gmail.com>
2021-04-14 15:38:14 -0300

    Without trycatch

--- middlewares/nameValidate.js
+++ middlewares/nameValidate.js
@@ -1,24 +1,24 @@
 const { UNPROCESSABLE_ENTITY } = require('../utils/statusCode.json');
 
-const nameValidate = async (req, res, next) => {
+const nameValidate = async (req, _res, next) => {
   const { name } = req.body;
   const err = new Error();
   err.code = 'invalid_data';
   err.statusCode = UNPROCESSABLE_ENTITY;
 
-  try {
-    const MIN_CHARACTERS = 5;
-    err.message = '"name" length must be at least 5 characters long';
-    if (name === undefined) {
-      next(err);
-      return;
-    }
-    if (name.length < MIN_CHARACTERS) next(err);
-
-    next();
-  } catch (error) {
-    next(error);
+  // try {
+  const MIN_CHARACTERS = 5;
+  err.message = '"name" length must be at least 5 characters long';
+  if (name === undefined) {
+    next(err);
+    return;
   }
+  if (name.length < MIN_CHARACTERS) next(err);
+
+  next();
+  // } catch (error) {
+  //   next(error);
+  // }
 };
 
 module.exports = nameValidate;
