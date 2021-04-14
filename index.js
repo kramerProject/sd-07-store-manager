86dc65a28ac6978ef3efd89799ec8068c68b4081
Andre Serudo <andreserudo@gmail.com>
2021-04-13 22:31:18 -0400

    requisitos de produtos

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const products = require('./routes/productRoute');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(products);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Na porta: ${PORT}`);
 });
