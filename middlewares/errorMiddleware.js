6030d19ca455c56c7377ad34c859c6a85f23d175
julioKauer <juliokauer@gmail.com>
2021-04-14 18:57:45 -0300

    Fix error Middleware

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -2,8 +2,8 @@ const { INTERNAL } = require('../utils/statusCode.json');
 const errorMiddleware = (err, _req, res, _next) => {
   res.status(INTERNAL);
   const { statusCode } = err;
-  if (statusCode) res.status(statusCode);
-  res.json({ err });
+  if (statusCode) return res.status(statusCode);
+  return res.json({ err });
 };
 
 module.exports = errorMiddleware;
