ad5b52772051cf14915e0116a05c3decf18d8a52
Felipe Nascimento <flpnascto@gmail.com>
2021-04-14 00:03:58 -0300

    Requisito 01

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
 
 // URL Local
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // URL Remoto
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
