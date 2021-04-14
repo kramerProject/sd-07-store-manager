b160de1bf2623ee9c5483fd82c56f6021017496b
julioKauer <juliokauer@gmail.com>
2021-04-14 19:03:24 -0300

    errorMiddleware commented

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,9 +1,9 @@
 const { INTERNAL } = require('../utils/statusCode.json');
 const errorMiddleware = (err, _req, res, _next) => {
-  res.status(INTERNAL);
-  const { statusCode } = err;
-  if (statusCode) return res.status(statusCode);
-  return res.json({ err });
+  // res.status(INTERNAL);
+  // const { statusCode } = err;
+  // if (statusCode) return res.status(statusCode);
+  // return res.json({ err });
 };
 
 module.exports = errorMiddleware;
