11cd088e8f92dd5be45adecab97ab50be4cb6c2c
Flavio Oliveira <flavsoliver@gmail.com>
2021-04-14 00:58:30 -0300

    URL mongobd

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const MONGO_DB_URL_LOCAL = 'mongodb://localhost:27017/StoreManager';
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = async () => {
   return MongoClient
-    .connect(MONGO_DB_URL_LOCAL, {
+    .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
