2bcc00ae045f8542961ede22b4dfb5ebe5ae0da6
Bruna Fonsêca <1998bfonseca@gmail.com>
2021-04-14 18:22:28 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen('3000', () => console.log('Rodando na porta 3000'));
