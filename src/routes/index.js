88d954345869713fa1ee84d7583587b5c72c3a79
Felipe Nascimento <flpnascto@gmail.com>
2021-04-14 11:38:43 -0300

    adicionar routes para Sales

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const routes = require('express').Router();
 
 const routeProducts = require('./productsRouter');
+const routeSales = require('./salesRouter');
 
 routes.use('/products', routeProducts);
+routes.use('/sales', routeSales);
 
 module.exports = routes;
