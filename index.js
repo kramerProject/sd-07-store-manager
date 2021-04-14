0d286a21717f1bdc12a583c3570be21dbf00ba28
Lfocosi <luccafocosi@gmail.com>
2021-04-14 18:12:12 -0300

    esqueci de implementar no index -_-

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const productRouter = require('./src/routers/product');
+const saleRouter = require('./src/routers/sale');
+const { sendError } = require('./src/configs/erro');
 
 const app = express();
 const PORT = 3000;
@@ -7,6 +10,12 @@ app.use(express.json());
 
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.use('/products', productRouter);
+app.use('/sales', saleRouter);
+app.use((err, _req, res, _next) => {
+  sendError(err, res);
 });
 
 app.listen(PORT);
