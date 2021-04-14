2c40e717d9160b19208059756e5b00e65103265e
julioKauer <juliokauer@gmail.com>
2021-04-14 15:04:52 -0300

    Fix undefined

--- middlewares/nameValidate.js
+++ middlewares/nameValidate.js
@@ -9,7 +9,10 @@ const nameValidate = async (req, res, next) => {
   try {
     const MIN_CHARACTERS = 5;
     err.message = '"name" length must be at least 5 characters long';
-    if (!name) next(err);
+    if (name === undefined) {
+      next(err);
+      return;
+    }
     if (name.length < MIN_CHARACTERS) next(err);
 
     next();
