5c52a9d5d67f58810b726893a790d5fb4623e95e
muriloPereira006 <muriloedu006@gmail.com>
2021-04-14 01:39:53 -0300

    coneccao com mongo falhando

--- config/conn.js
+++ config/conn.js
@@ -1,17 +1,16 @@
 const { MongoClient } = require('mongodb');
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
-const connection = () =>{
+connection = () =>{
   MongoClient.connect(MONGO_DB_URL, {
-    userNewUrlParser: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-    .then(conn => conn.db(DB_NAME))
+    .then((conn) => conn.db(DB_NAME))
     .catch(err => {
       console.error(err);
       process.exit(1);
--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,18 @@
-const SUCESS = 200;
+const connection = require('../config/conn');
+const { ObjectId } = require('mongodb');
 
-const constroller = async (req, res) => {
-  res.status(SUCESS).send({ menssage: 'teste easy' });
+const SUCESS = 200;
+const FAIL = 500;
+
+const controller = async (req, res) => {
+  try {
+    const result = await connection()
+      .then((db) => db.collection('products').find().toArray());
+    res.status(SUCESS).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(FAIL).json({ menssage: error.menssage });
+  }
 };
 
-module.exports = constroller;
+module.exports = controller;
