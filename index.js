72e3ec1ad7226a4c852d8b46c8017e47f08b6118
renatomak <renato.mark.silva@gmail.com>
2021-04-13 22:40:05 -0300

    iniciando o projeto 27

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
