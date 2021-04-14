754450fc0fdb2052818cef52daf5db12d9f3a39c
nevesfreire <nando.neves@gmail.com>
2021-04-13 21:40:33 -0300

    teste

--- controller/controllerProducts.js
+++ controller/controllerProducts.js
@@ -1,12 +1,13 @@
 const productsModel = require('../model/productsModel');
 const connection = require('../config/connection');
 const {validateName} = require('../service/productService');
-//falta fazer a validação
+
 const addProduct = async (req, res) => {
   const quinhentos = 500;
   const twoOhOne = 201;
   try {
     const { name, quantity } = req.body;
+    //fazendo validação
     validateName(name);
     const newItem = await productsModel.addItem(name, quantity); //adiciona ao db
     res.status(twoOhOne).json({newItem});
--- service/productService.js
+++ service/productService.js
@@ -1,33 +1,35 @@
 const connect = require('../config/connection');
 const five = 5;
+const zero = 0;
 const unprocessable_entity = 422;
 
 function validateName(name) {
+  // throw new Error('O campo "age" é obrigatório')
    
-    if (name.length < five) {
-        res.status(unprocessable_entity).send({
-            'err': {
-                'code': 'invalid_data',
-                'message': '"name" length must be at least 5 characters long'
-            }
-        });
-    }
+  if (name.length < five) {
+    throw new Error(JSON.stringify({
+      'err': {
+        'code': 'invalid_data',
+        'message': '"name" length must be at least 5 characters long'
+      }
+    }));
+  }
 
- connect().then(async (db) => {
-        const [productList] = await db.collection('products').find({ "name": name })
+  connect().then(async (db) => {
+    const [productList] = await db.collection('products').find({ 'name': name });
    
-     if (productList.length > 0) {
-        res.status(unprocessable_entity).send({
-            'err': {
-                'code': 'invalid_data',
-                'message': "Product already exists"
-            }
-        });
+    if (productList.length > zero) {
+      throw new Error (JSON.stringify({
+        'err': {
+          'code': 'invalid_data',
+          'message': 'Product already exists'
+        }
+      }));
     }
-    });
+  });
     
 }
 
 module.exports = {
-    validateName
+  validateName
 };
