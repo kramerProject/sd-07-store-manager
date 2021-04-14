43835583f0c39d254a2fc8a181cc5eddd3391b50
julioKauer <juliokauer@gmail.com>
2021-04-14 15:41:17 -0300

    without undefined test

--- middlewares/nameValidate.js
+++ middlewares/nameValidate.js
@@ -6,19 +6,15 @@ const nameValidate = async (req, _res, next) => {
   err.code = 'invalid_data';
   err.statusCode = UNPROCESSABLE_ENTITY;
 
-  // try {
   const MIN_CHARACTERS = 5;
   err.message = '"name" length must be at least 5 characters long';
-  if (name === undefined) {
-    next(err);
-    return;
-  }
+  // if (name === undefined) {
+  //   next(err);
+  //   return;
+  // }
   if (name.length < MIN_CHARACTERS) next(err);
 
   next();
-  // } catch (error) {
-  //   next(error);
-  // }
 };
 
 module.exports = nameValidate;
