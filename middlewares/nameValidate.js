03cb72f7693938637a2144bb120c621cb5bf8cf5
julioKauer <juliokauer@gmail.com>
2021-04-14 14:55:49 -0300

    Fix tryCatch

--- middlewares/nameValidate.js
+++ middlewares/nameValidate.js
@@ -6,11 +6,16 @@ const nameValidate = async (req, res, next) => {
   err.code = 'invalid_data';
   err.statusCode = UNPROCESSABLE_ENTITY;
 
-  const MIN_CHARACTERS = 5;
-  err.message = '"name" length must be at least 5 characters long';
-  if (name.length < MIN_CHARACTERS) next(err);
+  try {
+    const MIN_CHARACTERS = 5;
+    err.message = '"name" length must be at least 5 characters long';
+    if (!name) next(err);
+    if (name.length < MIN_CHARACTERS) next(err);
 
-  next();
+    next();
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = nameValidate;
