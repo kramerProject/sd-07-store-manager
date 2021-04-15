3c588bc036dd38c82b10af63a18bf353c4a602ca
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-04-15 08:19:29 -0300

    Iniciando Projeto - 15/04/2021

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+const SUCESS = 200;
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.send();
+  response.status(SUCESS).send({
+    message: 'PROJETO STORE MANAGER',
+  });
 });
+
+app.listen(PORT, () => console.log('App rodando na porta ' + PORT));
