7d4e058e04f01335cae1face2206f59469bfc911
nevesfreire <nando.neves@gmail.com>
2021-04-15 11:27:21 -0300

    100

--- controller/controllerSales.js
+++ controller/controllerSales.js
@@ -1,12 +1,12 @@
 const {
-  checkQuantities,
-  checkQuantity,
-  // checkIds,
-  addToSales,
-  serviceGetAllSales,
-  serviceGetSalesById,
-  serviceUpdateSalesById,
-  serviceDeleteSalesById
+    checkQuantities,
+    // checkQuantity,
+    // checkIds,
+    addToSales,
+    serviceGetAllSales,
+    serviceGetSalesById,
+    serviceUpdateSalesById,
+    serviceDeleteSalesById
 } = require('../service/salesService');
 const unprocessable_entity = 422;
 const success = 200;
@@ -14,71 +14,71 @@ const not_found = 404;
 
 //   const addProduct = async (req, res) => {
 const controllerAddSales = async (req, res) => {
-  const salesList = req.body;
-  try {
-    //  await checkIds(salesList); não precisa disso?
-    checkQuantities(salesList);
-    const result = await addToSales(salesList);
-    return res.status(success).json(result);
-  } catch (err) {
-    res.status(unprocessable_entity).json({
-      'err': { 'code': 'invalid_data', 'message': err.message }
-    });
-  }
+    const salesList = req.body;
+    try {
+        //  await checkIds(salesList); não precisa disso?
+        checkQuantities(salesList);
+        const result = await addToSales(salesList);
+        return res.status(success).json(result);
+    } catch (err) {
+        res.status(unprocessable_entity).json({
+            'err': { 'code': 'invalid_data', 'message': err.message }
+        });
+    }
 };
 
 const getAllSales = async (req, res) => {
-  try {
-    const salesList = await serviceGetAllSales();
-    return res.status(success).json({ sales: salesList });
-  } catch (err) {
-    console.log(err.message);
-  }
+    try {
+        const salesList = await serviceGetAllSales();
+        return res.status(success).json({ sales: salesList });
+    } catch (err) {
+        console.log(err.message);
+    }
 };
 
 const getSalesById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const result = await serviceGetSalesById(id);
-    return res.status(success).json(result);
-  } catch (err) {
-    res.status(not_found).json({
-      'err': {
-        'code': 'not_found', 'message': err.message
-      }
-    });
-  }
+    const { id } = req.params;
+    try {
+        const result = await serviceGetSalesById(id);
+        return res.status(success).json(result);
+    } catch (err) {
+        res.status(not_found).json({
+            'err': {
+                'code': 'not_found', 'message': err.message
+            }
+        });
+    }
 };
 
 const updateSalesById = async (req, res) => {
-  const { salesId } = req.params;
-  try {
-    const productList = req.body;
-    checkQuantities(productList);
-    const newItem = await serviceUpdateSalesById(salesId, productList);
-    return res.status(success).json(newItem);
-  } catch (err) {
-    res.status(unprocessable_entity).json({
-      'err': { 'code': 'invalid_data', 'message': err.message }
-    });
-  }
+    const { salesId } = req.params;
+    try {
+        const productList = req.body;
+        checkQuantities(productList);
+        const newItem = await serviceUpdateSalesById(salesId, productList);
+        return res.status(success).json(newItem);
+    } catch (err) {
+        res.status(unprocessable_entity).json({
+            'err': { 'code': 'invalid_data', 'message': err.message }
+        });
+    }
 };
 
 const deleteSalesById = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const result = await serviceDeleteSalesById(id);
-    if (result) return res.status(success).json(result);
-  } catch (err) {
-    res.status(unprocessable_entity).json({
-      'err': { 'code': 'invalid_data', 'message': err.message }
-    });
-  }
+    const { id } = req.params;
+    try {
+        const result = await serviceDeleteSalesById(id);
+        if (result) return res.status(success).json(result);
+    } catch (err) {
+        res.status(unprocessable_entity).json({
+            'err': { 'code': 'invalid_data', 'message': err.message }
+        });
+    }
 };
 module.exports = {
-  controllerAddSales,
-  getAllSales,
-  getSalesById,
-  updateSalesById,
-  deleteSalesById
+    controllerAddSales,
+    getAllSales,
+    getSalesById,
+    updateSalesById,
+    deleteSalesById
 };
