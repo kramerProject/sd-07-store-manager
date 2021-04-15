edb69d730cd7ce5dd9bdb3e1d2ded29a7527214a
TIAGO <tiagoesdrasvieira@gmail.com>
2021-04-15 15:13:10 -0300

    Teste das funcoes

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,54 +1,57 @@
 const statusCode = require('../../helpers/HTTPStatus');
 
 const getAllProducts = async (_req, res) => {
-    try {
-        res.status(statusCode.SUCCESS).json({message: "funcionou"});
-    } catch (err) {
-        console.error(err.message)
-        res.status(statusCode.INTERNALERROR).json({ message: err.message});        
-    }
-}
+  try {
+    res.status(statusCode.SUCCESS).json({message: 'funcionou'});
+  } catch (err) {
+    console.error(err.message);
+    res.status(statusCode.INTERNALERROR).json({ message: err.message});        
+  }
+};
 
-const getProductById = async (_req, res) => {
-    try {
-        res.status(statusCode.SUCCESS).json({message: "funcionou"});
-    } catch (err) {
-        console.error(err.message)
-        res.status(statusCode.INTERNALERROR).json({ message: err.message});        
-    }
-}
+const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(statusCode.SUCCESS).json({id});
+  } catch (err) {
+    console.error(err.message);
+    res.status(statusCode.INTERNALERROR).json({ message: err.message});        
+  }
+};
 
 const createProduct = async (_req, res) => {
-    try {
-        res.status(statusCode.SUCCESS).json({message: "funcionou"});
-    } catch (err) {
-        console.error(err.message)
-        res.status(statusCode.INTERNALERROR).json({ message: err.message});        
-    }
-}
+  try {
+    res.status(statusCode.CREATED).json({message: 'criado'});
+  } catch (err) {
+    console.error(err.message);
+    res.status(statusCode.INTERNALERROR).json({ message: err.message});        
+  }
+};
 
-const updateProduct = async (_req, res) => {
-    try {
-        res.status(statusCode.SUCCESS).json({message: "funcionou"});
-    } catch (err) {
-        console.error(err.message)
-        res.status(statusCode.INTERNALERROR).json({ message: err.message});        
-    }
-}
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(statusCode.SUCCESS).json({message: `${id} atualizado`});
+  } catch (err) {
+    console.error(err.message);
+    res.status(statusCode.INTERNALERROR).json({ message: err.message});        
+  }
+};
 
-const deleteProduct = async (_req, res) => {
-    try {
-        res.status(statusCode.SUCCESS).json({message: "funcionou"});
-    } catch (err) {
-        console.error(err.message)
-        res.status(statusCode.INTERNALERROR).json({ message: err.message});        
-    }
-}
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    res.status(statusCode.SUCCESS).json({message: `${id} deletado`});
+  } catch (err) {
+    console.error(err.message);
+    res.status(statusCode.INTERNALERROR).json({ message: err.message});        
+  }
+};
 
 module.exports = {
-    getAllProducts,
-    getProductById,
-    createProduct,
-    updateProduct,
-    deleteProduct
-}
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct
+};
