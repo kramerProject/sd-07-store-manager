61066a7de22c8d00b413116114a90980958baa60
ilanribeiro <ilanribeiro@gmail.com>
2021-04-14 22:39:40 -0300

    [ Feature ] Creates the index.js file with the first routes for products and sales.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+const port = 3000;
+
+dotenv.config();
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+// app.use('/products', ProductsRouter);
+// app.use('sales', SalesRouter);
+
+app.get('/teste', (_req, res) => res.send('Hello Project'));
+app.listen(port, () => console.log(`MyStoreApp active and listening on port ${port}`));
