40ea1c92575c14d7d17f179b1854d7dece0e0acd
Filipi Firmino <contatoic3web@gmail.com>
2021-04-14 21:22:08 -0300

    connection correto

--- src/Models/connection.js
+++ src/Models/connection.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 // URL para teste remoto.
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
 
 // URL ENV para teste local. 
 // process.env.DB_URL_TEST
@@ -15,7 +16,7 @@ const options = {
 const connection = () => {
   return MongoClient
     .connect(MONGO_DB_URL, options)
-    .then((conn) => conn.db(process.env.DB_NAME))
+    .then((conn) => conn.db(DB_NAME))
     .catch((err) => {
       console.log(`Erro na conexão do banco: ${err}`);
       process.exit();
