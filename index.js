1bbe52220715caad687ca4deaae097f5a0a41cc6
Galaraz <jvictor.si.2013@gmail.com>
2021-04-15 21:35:49 -0300

    init no projeto

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+const express = require('express');
+
+const { ProductRoute, SalesRoute } = require('./Routes');
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', ProductRoute);
+
+app.use('/sales', SalesRoute);
+
+
+app.listen(port, () => console.log('Projeto Online na Porta ' + port));
