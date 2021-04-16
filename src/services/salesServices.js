c680f55f2ddaa131d055d0bd523dd8bc5a5b24b9
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-04-16 19:41:32 -0300

    req 9 - 16/04/2021

--- src/services/salesServices.js
+++ src/services/salesServices.js
@@ -1,14 +1,33 @@
 const { ObjectID } = require('mongodb');
-const { salesModel } = require('../models');
+const { salesModel, productsModel } = require('../models');
 const { create, read, readById, update, exclude } = salesModel;
+
+const updateQuantity = async (saleList, operation) => {
+  const ProductPromises = saleList.map((sale) => productsModel.readById(sale.productId));
+  const ProductList = await Promise.all(ProductPromises);
+
+  await Promise.all(
+    ProductList.map((product, index) => {
+      const { _id, name, quantity } = product;
+      let quantityProduct = quantity;
+      let quantitySale = saleList[index].quantity;
+      let newQuantity =
+        operation === 'subtract' ? quantityProduct - quantitySale : quantityProduct + quantitySale;
+      console.log('atualizou');
+      return productsModel.update(_id, name, newQuantity);
+    }),
+  );
+};
 
 const createSale = async (productList) => {
   productList.forEach((product) => {
-    if (!ObjectID.isValid(product.productId)) throw new Error('Wrong product ID or invalid quantity');
+    if (!ObjectID.isValid(product.productId))
+      throw new Error('Wrong product ID or invalid quantity');
     if (product.quantity <= 0 || typeof product.quantity !== 'number')
       throw new Error('Wrong product ID or invalid quantity');
   });
 
+  await updateQuantity(productList, 'subtract');
   const newSale = await create(productList);
   if (!newSale.result.ok) throw new Error('Error from model - create');
   return { _id: newSale.insertedId, itensSold: productList };
@@ -44,6 +63,7 @@ const deleteSaleById = async (id) => {
   const readSale = await readSalesById(id);
   const SaleDeleted = await exclude(id);
   if (!SaleDeleted.result.ok) throw new Error('Error from model - deleteSaleById');
+  await updateQuantity(readSale.itensSold, 'add');
   return readSale;
 };
 
