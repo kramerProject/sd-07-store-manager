2314732fe05863d6561d5ee87f8aadd83def9cc0
Pedro Risso <pmerchiorattor@gmail.com>
2021-04-17 08:29:54 -0300

    fix: change connection URL

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,12 +1,10 @@
 const mongoClient = require('mongodb').MongoClient;
 
 // Teste local:
-
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // Teste avaliador:
-
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 
