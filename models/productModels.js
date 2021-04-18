a71bd7f9dc2c9fa0db54ea1d6450bff894699c34
muriloPereira006 <muriloedu006@gmail.com>
2021-04-18 03:21:12 -0300

    post create Item funcionando

--- models/productModels.js
+++ models/productModels.js
@@ -4,16 +4,26 @@ const { ObjectId } = require('mongodb');
 
 
 const validationFail = (name, quantity, nameOfProduct) => {
-  if(name.length < 5) return '"name" length must be at least 5 characters long';
+  const minLengthName = 5;
+  const minQuantity = 0;
+
+  if (quantity <= minQuantity) return '"quantity" must be larger than or equal to 1';
+  if (typeof(quantity) !== 'number') return '"quantity" must be a number';
+
   if (nameOfProduct) return 'Product already exists';
+  if(name.length < minLengthName) {
+    return '"name" length must be at least 5 characters long';
+  };
+
+  return undefined;
 };
 
 const createProducts = async (name, quantity) => {
   const nameOfProduct = await connection()
-    .then((db) => db.collection('products').find({ name }));
+    .then((db) => db.collection('products').findOne({ name }));
 
   const validationsFail = await validationFail(name, quantity, nameOfProduct);
-  if (validationsFail !== undefined) throw new Error(validationsFail);
+  if (validationsFail != undefined) throw new Error(validationsFail);
 
   const result = await connection()
     .then((db) => db.collection('products').insertOne({ name, quantity }))
