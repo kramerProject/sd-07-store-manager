5206612edac15c5b785f0b2ccf078d890439ca63
muriloPereira006 <muriloedu006@gmail.com>
2021-04-18 03:05:46 -0300

    agora sim passou a msg!!

--- models/productModels.js
+++ models/productModels.js
@@ -1,7 +1,20 @@
 const connection = require('../config/conn');
 const { ObjectId } = require('mongodb');
 
+
+
+const validationFail = (name, quantity, nameOfProduct) => {
+  if(name.length < 5) return '"name" length must be at least 5 characters long';
+  if (nameOfProduct) return 'Product already exists';
+};
+
 const createProducts = async (name, quantity) => {
+  const nameOfProduct = await connection()
+    .then((db) => db.collection('products').find({ name }));
+
+  const validationsFail = await validationFail(name, quantity, nameOfProduct);
+  if (validationsFail !== undefined) throw new Error(validationsFail);
+
   const result = await connection()
     .then((db) => db.collection('products').insertOne({ name, quantity }))
     .then(response => response.ops[0]);
--- service/index.js
+++ service/index.js
@@ -1,17 +1,6 @@
 const productModel = require('../models/productModels');
 
-const validationFail = (name, quantity, nameOfProduct) => {
-  if(name.length < 5) return '"name" length must be at least 5 characters long';
-  if (nameOfProduct) return 'Product already exists';
-};
-
 const createProducts = async (name, quantity) => {
-  const nameOfProduct = await connection()
-    .then((db) => db.collection('products').find({ name }));
-
-  const validationsFail = await validationFail(name, quantity, nameOfProduct);
-  if (validationsFail != undefined) throw new Error(validationsFail);
-  
   const response = await productModel.createProducts(name, quantity);
   return response;
 };
