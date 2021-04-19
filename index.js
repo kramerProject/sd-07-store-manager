6575d9d635ae69e9e0b6d163911a0f1a003dc87e
jvitorof <j.vitoroflores@gmail.com>
2021-04-19 00:09:47 -0300

    :rocket: Creating bases :house_with_garden:

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require('express');
+const routes = require('./routes');
+
+const port = 3000;
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.use(express.json());
+app.use(routes.productRoutes);
+app.use(routes.saleRoutes);
+
+app.listen(port, () => {
+  console.log(`ouvindo a porta ${port}!`);
 });
