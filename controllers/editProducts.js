168f7d3b96dc066504a253e5f15f2583957a45d1
Strongreen <strongreen@live.com>
2021-04-21 14:50:41 -0300

    correçao do put inteiro

--- controllers/editProducts.js
+++ controllers/editProducts.js
@@ -4,7 +4,7 @@ const { SUCCESS, ENTITY} = require('../CODE_ERROR');
 async function editProducts(req, res) {
   try {
     const { id } = req.params;
-    const { name, quantity } = req.params;
+    const { name, quantity } = req.body;
     const data = await editProduct(id, name, quantity);
     res.status(SUCCESS).json(data);
   } catch (error) {
