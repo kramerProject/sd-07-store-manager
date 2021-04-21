817654330de4e455bef337b8644c8266ee1c1385
Muniz <vinicius.muniz.marin@gmail.com>
2021-04-21 14:36:03 -0300

    Iniciando projeto

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/products', (req, res) => {
+
+})
+
+app.listen(3000, () => {
+  console.log('App rodando na porta 3000');
+})
