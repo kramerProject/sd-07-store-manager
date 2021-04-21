a2219001d82f6c27cbd54ef5db57d803ff70396d
Strongreen <strongreen@live.com>
2021-04-21 18:32:20 -0300

    correçao na captura do edit

--- controllers/editSale.js
+++ controllers/editSale.js
@@ -4,8 +4,8 @@ const { SUCCESS, ENTITY} = require('../CODE_ERROR');
 async function editSale(req, res) {
   try {
     const { id } = req.params;
-    const { name, quantity } = req.params;
-    const data = await editSales(id, name, quantity);
+    const sale = req.body;
+    const data = await editSales(id, sale);
     res.status(SUCCESS).json(data);
   } catch (error) {
     res.status(ENTITY).json({ 
