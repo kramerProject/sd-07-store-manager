674ada88035f9db7f50c60eae9796e37ccab5a4d
Arthur de Sa <arthurdesa94@gmail.com>
2021-04-22 13:41:15 -0300

    pelamor

--- src/middlewares/productName.js
+++ src/middlewares/productName.js
@@ -1,12 +1,20 @@
 const codes = require('../services/codes');
 const productNameMid = (req, res, next) => {
-  const { name } = req.body;
-  const five = 5;
-  if (!name || name.length < five) {
+  const { name, quantity } = req.body;
+
+  if (name.length < number) {
     return res.status(codes.unprocessableEntity).json({
       err: {
         code: 'invalid_data',
         message: '"name" length must be at least 5 characters long',
+      },
+    });
+  }
+  if (quantity < 1) {
+    return res.status(codes.unprocessableEntity).json({
+      err: {
+        code: 'invalid_data',
+        message: '"quantity" must be larger than or equal to 1',
       },
     });
   }
