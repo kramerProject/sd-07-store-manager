216f1ea400a55138f120b4a00cd2099a55f50f72
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-04-22 23:47:10 -0300

    First commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || '3000';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log('O pai tá on!'));
