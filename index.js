ae7867a346112f66ff414537eef8e3e274f4b7d0
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2021-04-24 21:57:25 -0300

    Portal local 3000

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || '3000';
+const LOCAL_PORT = 3000;
+
+const PORT = process.env.PORT || LOCAL_PORT;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
