425038578623bcf47939824f65573f96cfaa64fc
Luiz Mariz <lfsmariz@gmail.com>
2021-04-25 16:45:46 -0300

    define middlewares

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+require('dotenv').config();
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.get('/', (request, response) => {
   response.send();
 });
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
