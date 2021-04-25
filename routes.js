7d88a8a4f36b20548ac0f67e7e22256d53cad63c
Luiz Mariz <lfsmariz@gmail.com>
2021-04-25 16:44:52 -0300

    define routes

---
+++ routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const Products = require('./controllers/Products');
+const Sales = require('./controllers/Sales');
+
+const router = express.Router();
+//products routes
+router.get('/products', Product.findAllProducts);
+router.get('/products/:id', Product.findProductById);
+router.post('/products', Product.addProduct);
+router.put('/products/:id', Product.updateProduct);
+router.delete('/products/:id', Product.deleteProduct);
+//sales routes
+router.get('/sales', Sale.findAllSales);
+router.get('/sales/:id', Sale.findSaleById);
+router.post('/sales', Sale.addSales);
+router.put('/sales/:id', Sale.updateSale);
+router.delete('/sales/:id', Sale.deleteSale);
+
+module.exports = router;
