aa3aecefa3aa862fd03755fca57583c40e347492
Eriquy Jhordan <eriquy@gmail.com>
2021-05-14 15:56:42 -0300

    create serve and structure of files

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+
+const app = express();
+
+const PORT =  3333;
+
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(process.env.PORT || PORT, () => (console.log('Rodando...')));
