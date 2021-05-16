6ada5f72cb1f4ea99fd3883a0ab29c9aa137068e
Felipe Farina <felipe@pop-os.localdomain>
2021-05-16 11:45:44 -0300

    requisit bonus

--- routes/saleRoutes.js
+++ routes/saleRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const saleController = require('../controller/saleController');
+const verifyStock = require('../middlewares/verifyStock');
 
 const router = express.Router();
 
-router.post('/sales', saleController.insertSale);
+router.post('/sales', verifyStock, saleController.insertSale);
 router.get('/sales', saleController.findAll);
 router.get('/sales/:id', saleController.findSaleById);
 router.put('/sales/:id', saleController.updateSaleById);
