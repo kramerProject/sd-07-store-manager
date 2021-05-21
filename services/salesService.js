738ff1a8765374515bc59eb4df77b1bd6001592c
Jr3564 <rodolfojr94@gmail.com>
2021-05-21 16:51:48 -0300

    requisito 10

--- services/salesService.js
+++ services/salesService.js
@@ -18,14 +18,14 @@ const getById = async (id) => {
 const create = async (itensSold) => {
   salesValidate.quantity(itensSold);
   await salesValidate.stock(itensSold);
-  // await productService.updateQuantityStock(itensSold);
+  await productService.updateQuantityStock(itensSold);
   return salesModel.create(itensSold);
 };
 
 const update = async (id, itensSold) => {
   salesValidate.quantity(itensSold);
   await salesValidate.stock(itensSold);
-  // await productService.updateQuantityStock(itensSold);
+  await productService.updateQuantityStock(itensSold);
   return salesModel.update(id, itensSold);
   
 };
@@ -33,8 +33,16 @@ const update = async (id, itensSold) => {
 const exclude = async (id) => {
   salesValidate.id(id);
   const ONE = 1;
+  // return salesModel.exclude(id);
   // await productService.updateQuantityStock(itensSold, ONE);
-  return salesModel.exclude(id);
+  return salesModel.exclude(id)
+    .then(async (result) => {
+      await productService.updateQuantityStock([
+        { _id: result._id, quantity: result.quantity }
+      ], ONE);
+      console.log(result.documents);
+      return result;
+    });
 };
 
 module.exports = { getAll, getById, create, update, exclude };
