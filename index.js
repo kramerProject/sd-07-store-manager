a090af5ad88f53dd30650ca3d943fffdbeac00c8
vladimirrchagas <vladimirrchagas@gmail.com>
2021-05-21 18:33:35 -0300

    First commit to start project store manager

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`Servidor Online na porta ${PORT}`));
