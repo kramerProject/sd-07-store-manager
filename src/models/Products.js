8340afa3334f5e2bca9f540f39f4d844353de388
Eduardo Pizorno <pizorno@gmial.com>
2021-05-21 18:48:28 -0300

    Fix project url mongo

--- src/models/Products.js
+++ src/models/Products.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const create = async (name, quantity) =>
   connection()
-    .then ((db) => db.collection('products').insertOne ({name, quantity }));
+    .then ((db) => db.collection('products').insertOne ({ name, quantity }));
 
 const getAll = async () => 
   connection() 
